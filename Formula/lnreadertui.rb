6eefe12c684572be60c18a43b39d36388fc99ab5
hhdtc <alex_hhdtc@outlook.com>
2026-08-27 09:24:04 -0500

    lnreadertui 1.1.2 tap formula

---
+++ Formula/lnreadertui.rb
@@ -0,0 +1,13 @@
+class Lnreadertui < Formula
+  desc "Terminal reader for bilinovel.com light novels: search, download as EPUB, read with auto progress"
+  homepage "https://github.com/hhdtc/LNReaderTUI"
+  url "https://github.com/hhdtc/LNReaderTUI/archive/refs/tags/v1.1.2.tar.gz"
+  sha256 "fa2001dd7058ad8b050d5c0b01f5cb313a5b4ada0d9b1d3fbf565276f8cffbf5"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-trimpath", "-ldflags", "-s -w", "-o", bin/"lnreadertui", "."
+  end
+end
---
+++ README.md
@@ -0,0 +1,14 @@
+# homebrew-tap
+
+Homebrew tap for [LNReaderTUI](https://github.com/hhdtc/LNReaderTUI).
+
+```bash
+brew tap hhdtc/tap
+brew install lnreadertui
+```
+
+Upgrade:
+
+```bash
+brew upgrade lnreadertui
+```
